06f952046492c2ccaf227f0812792dbc7005efd6
anastasiialepilina123 <a.lepilina@internet.ru>
2023-10-14 22:03:42 +0300

    hhhgf

---
+++ Class1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    public class Class1
+    {
+
+        public static void clear()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write("\r" + new string(' ', Console.BufferWidth) + "\r");
+        }
+        public static int Strelka()
+        {
+            int position = 1;
+            ConsoleKeyInfo key;
+
+            do
+            {
+                Console.SetCursorPosition(0, position);
+                Console.WriteLine("->");
+
+                key = Console.ReadKey();
+
+                Console.SetCursorPosition(0, position);
+                Console.WriteLine(" ");
+
+                if (key.Key == ConsoleKey.UpArrow && position != 1)
+                {
+                    position--;
+                }
+                else if (key.Key == ConsoleKey.DownArrow && position != 3)
+                {
+                    position++;
+                }
+
+            } while (key.Key != ConsoleKey.Enter);
+
+            return position;
+        }
+        public static void Zapisi(int position, string date)
+        {
+            if (position == 1)
+            {
+                Console.SetCursorPosition(0, 6);
+                clear();
+
+                Console.SetCursorPosition(0, 5);
+                if (date == "13.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Название: Тренировка");
+                }
+                else if (date == "14.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Название: Купить продукты");
+                }
+                else if (date == "15.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Название: Помыть окна");
+                }
+
+                Console.SetCursorPosition(0, 8);
+
+                clear();
+
+                Console.SetCursorPosition(0, 7);
+                if (date == "13.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Описание: Нужно сходить на тренировку по йоге");
+                }
+                else if (date == "14.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Описание: Нужно купить молоко и хлеб");
+                }
+                else if (date == "15.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Описание: Нужно вымыть окна в зале");
+                }
+
+                Console.SetCursorPosition(0, 10);
+                clear();
+
+                Console.SetCursorPosition(0, 9);
+            }
+
+            else if (position == 2)
+            {
+                Console.SetCursorPosition(0, 6);
+                clear();
+
+                Console.SetCursorPosition(0, 5);
+                if (date == "13.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Название: Чтение");
+                }
+                else if (date == "14.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Название: Сходить к бабушке");
+                }
+                else if (date == "15.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Название: Починить замок");
+                }
+
+                Console.SetCursorPosition(0, 8);
+
+                clear();
+
+                Console.SetCursorPosition(0, 7);
+                if (date == "13.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Описание: Нужно почитать книгу");
+                }
+                else if (date == "14.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Описание: Нужно сходить к бабушке");
+                }
+                else if (date == "15.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Описание: Нужно починить замок от кухни");
+                }
+
+                Console.SetCursorPosition(0, 10);
+                clear();
+
+                Console.SetCursorPosition(0, 9);
+
+            }
+            else if (position == 3)
+            {
+                Console.SetCursorPosition(0, 6);
+                clear();
+
+                Console.SetCursorPosition(0, 5);
+                if (date == "13.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Название: Уборка");
+                }
+                else if (date == "14.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Название: Погулять с собакой");
+                }
+                else if (date == "15.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Название: Сделать уроки");
+                }
+
+                Console.SetCursorPosition(0, 8);
+
+                clear();
+
+                Console.SetCursorPosition(0, 7);
+                if (date == "13.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Описание: Нужно убрать свою комнату");
+                }
+                else if (date == "14.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Описание: Нужно выгулять песика");
+                }
+                else if (date == "15.10.2023 0:00:00")
+                {
+                    Console.WriteLine("Описание: Нужно сделать уроки");
+                }
+
+                Console.SetCursorPosition(0, 10);
+                clear();
+
+                Console.SetCursorPosition(0, 9);
+
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿
+using ConsoleApp17;
+
+namespace ConsoleApp17
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            DateTime currentDate = DateTime.Today;
+
+
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Выбрана дата:" + currentDate.ToString());
+
+                Console.SetCursorPosition(0, 2);
+                Class1.clear();
+                if (currentDate.ToString() == "13.10.2023 0:00:00")
+                {
+
+                    Console.WriteLine("  1.Тренировка");
+                    Console.WriteLine("  2.Чтение");
+                    Console.WriteLine("  3.Уборка");
+                }
+                else if (currentDate.ToString() == "14.10.2023 0:00:00")
+                {
+                    Console.WriteLine("  1.Купить продукты");
+                    Console.WriteLine("  2.Сходить к бабушке");
+                    Console.WriteLine("  3.Погулять с собакой");
+                }
+
+                else if (currentDate.ToString() == "15.10.2023 0:00:00")
+                {
+                    Console.WriteLine("  1.Помыть окна");
+                    Console.WriteLine("  2.Починить замок");
+                    Console.WriteLine("  3.Сделать уроки");
+                }
+
+                var key = Console.ReadKey().Key;
+                if (key == ConsoleKey.RightArrow)
+                {
+                    currentDate = currentDate.AddDays(1);
+                    Console.Clear();
+
+
+                }
+                else if (key == ConsoleKey.LeftArrow)
+                {
+                    currentDate = currentDate.AddDays(-1);
+                    Console.Clear();
+                }
+                else if (key == ConsoleKey.UpArrow || key == ConsoleKey.DownArrow)
+                {
+                    Class1.Zapisi(Class1.Strelka(), currentDate.ToString());
+
+                }
+
+
+
+
+
+            }
+        }
+
+    }
+}
+
